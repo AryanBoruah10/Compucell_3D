2d06ac5bc3685dec59489e519e88ca14a8cb23cd
Aryan <94703869+AryanBoruah10@users.noreply.github.com>
2025-01-11 20:17:34 +0530

    Update CPM_circularSteppables.py

--- CPM_circular/Simulation/CPM_circularSteppables.py
+++ CPM_circular/Simulation/CPM_circularSteppables.py
@@ -5,9 +5,15 @@ class CPM_circularSteppable(SteppableBasePy):
 
     def __init__(self, frequency=1):
 
-        SteppableBasePy.__init__(self,frequency)
+        SteppableBasePy.__init__(self, frequency)
+        
+
 
     def start(self):
+        center_x = int(self.dim.x / 2)  # Calculate center X
+        center_y = int(self.dim.y / 2)  # Calculate center Y
+        cell = self.new_cell(self.SC)  # Create an SC cell
+        self.cell_field[center_x, center_y, 0] = cell
         """
         Called before MCS=0 while building the initial simulation
         """
